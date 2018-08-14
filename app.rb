fbbf213415b8592865a538f87528242774e8edc6
maruleba <folaa1234@gmail.com>
2018-08-14 17:18:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ class App < Sinatra::Base
   get "/name" do
   "Hello my name is Fola!"
 end 
+get "/hometown" do
+  "My hometown is The Bronx"
+end
 
-
+get "/favorite-food" do
+  "My favorite-food is pizza"
 end
